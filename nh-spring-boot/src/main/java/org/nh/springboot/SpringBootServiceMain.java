e058841e0b783db1034d93c10a950150d2698658
yindanqing <yindanqing925@163.com>
2019-05-05 14:07:37 +0800

    删除多余引用;

--- nh-spring-boot/src/main/java/org/nh/springboot/SpringBootServiceMain.java
+++ nh-spring-boot/src/main/java/org/nh/springboot/SpringBootServiceMain.java
@@ -2,7 +2,6 @@ package org.nh.springboot;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.context.annotation.ComponentScan;
