c3441d4b251ac1d0706b44bf7003da4aff2081ef
yindanqing <yindanqing925@163.com>
2019-05-17 03:15:18 +0800

    新增页面设置，添加页面支持；

---
+++ nh-spring-boot/src/main/java/org/nh/springboot/config/IndexConfig.java
@@ -0,0 +1,23 @@
+package org.nh.springboot.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.Ordered;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+/**
+ * @author yindanqing
+ * @date 2019/5/17 2:58
+ * @description 默认请求跳转控制
+ */
+@Configuration
+public class IndexConfig extends WebMvcConfigurerAdapter {
+
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("/").setViewName("index");
+        registry.setOrder(Ordered.HIGHEST_PRECEDENCE);
+        super.addViewControllers(registry);
+    }
+
+}
